db6011a9c30574ea1d161e045947aa30e4860df5
Dusuna <dusunax@gmail.com>
2022-08-08 19:40:57 +0900

    add TextInput, Button for /write

--- src/pages/WritePage.jsx
+++ src/pages/WritePage.jsx
@@ -1,7 +1,59 @@
+import Button from "../components/Button";
+import TextInputBox from "../components/TextInputBox";
+import styled from "styled-components";
+
 export default function WritePage (){
+    
     return (
-        <>
-            WritePage
-        </>
+        <MyForm>
+            <TextInputBox 
+            name={'username'} 
+            placeholder={'작성자'}
+            max={20}
+            required
+            />
+
+            <TextInputBox 
+            name={'title'} 
+            placeholder={'제목'}
+            max={50}
+            required
+            />
+
+            <TextInputBox 
+            name={'chk1'} 
+            placeholder={'첫 번째 체크 박스'}
+            max={100}
+            required
+            />
+
+            <TextInputBox 
+            name={'chk2'} 
+            placeholder={'두 번째 체크 박스'}
+            max={100}
+            />
+
+            <TextInputBox 
+            name={'chk3'} 
+            placeholder={'세 번째 체크 박스'}
+            max={100}
+            />
+
+            <div>
+                <Button 
+                buttonText={'클릭하세요'} 
+                action={null}
+                />
+            </div>
+
+        </MyForm>
     )
 }
+
+const MyForm = styled.form`
+
+& > *:last-child {
+    margin-top: 40px;
+}
+
+`
