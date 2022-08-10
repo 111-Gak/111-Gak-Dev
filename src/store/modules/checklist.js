761e3436c85e0a7bb21dab619068f5b4fc2c98e7
Dusuna <dusunax@gmail.com>
2022-08-10 14:51:56 +0900

    issue: checkbox 404 에러

--- src/store/modules/checklist.js
+++ src/store/modules/checklist.js
@@ -1,23 +1,47 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
 
 const initialState = {
     checklist: {
         chk1Text: ""
     }
 }
+export const patchChecklistThunk = createAsyncThunk(
+    "patch",
+    async (payload, api) => {
+        console.log(payload, api);
+        try {
+            const data = await axios.patch("http://localhost:3001/checklist?postId=6f-s9ewv0n", payload)
+            return data
+            // return api.fulfillWithValue(data.data)
+        } catch(err) {
+            return api.rejectWithValue(err)
+        }
+    } 
+)
 
 export const checklistSlice = createSlice({
     name: "checklists",
-    initialState,
+    initialState: { list: [{ id: '3423423423t6sd', chkchk: 0 }] },
     reducers: {
         editChecklist: (state, action) => {
             const {idx: idx, currentValue} = action.payload;
             const newState={...state.checklist, "chkText": currentValue, idx: idx};
             
-            console.log(newState);
+            // console.log(newState);
 
             state.checklist = newState;
+        },
+        patchChecklist: (state, action) => {
         }
+    },
+    extraReducers: (builder) => {
+        builder.addCase(patchChecklistThunk.fulfilled, (state, action) => {
+            const newList = state.list.map((el) => {
+                return el.id === action.payload.id ? action.payload : el;
+            })
+            state.list = newList;
+        })
     }
 })
 
