2aada6ef9fafd52e82fd2e70df2d07ee0d518107
Dusuna <dusunax@gmail.com>
2022-08-10 18:58:38 +0900

    반짝임+에러 현상 fix
    데이터가 서버에서 load되었을 때만 출력하게 설정

--- src/pages/CommunityPage.jsx
+++ src/pages/CommunityPage.jsx
@@ -5,25 +5,33 @@ import Post from "../components/Post"
 
 export default function CommunityPage() {
     const [posts, setPosts] = useState([]);
-    
-    const fetchPosts = async () => {
-        const { data } = await axios.get("http://localhost:3001/posts")
-        
-        setPosts( data );
-    }
+    const [loaded, setLoaded] = useState(false);
     
     useEffect(()=>{
         fetchPosts()
-    }, [])
+    }, [loaded])
+
+    const fetchPosts = async () => {
+        let data;
+        try {
+            data = await axios.get("http://localhost:3001/posts")
+        } catch (err) {
+            console.log(err)
+        } finally {
+            setPosts(data.data)
+            setLoaded(true)
+        }
+    }
 
     return (
         <>
-            {posts.map(post => {
-                return <Post 
-                list={{...post}} 
-                key={post.postId}
-                />
-            })}
+            {loaded && posts.map(post => {
+                    return <Post 
+                    list={{...post}} 
+                    key={post.postId}
+                    />
+                })
+            }
         </>
     )
 }
